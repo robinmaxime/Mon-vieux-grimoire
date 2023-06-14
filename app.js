b7fb63a6153a553cf7e3f2d51085b22464b52437
Maxime ROBIN <robin.maxime44@gmail.com>
2023-06-14 11:00:34 +0200

    Setup Mongoose (connection + models)

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bookRouter = require("./routes/bookRouter");
+
 
 const app = express();
 
-// Autorise les reqêtes Cross Origin
+// Connexion à la base de donnée MongoDB
+mongoose.connect('mongodb+srv://robinmaxime44:nyxgex-goJno4-wofhew@cluster0.8ya4put.mongodb.net/monvieuxgrimoire?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+// Autorise les requêtes Cross Origin
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,6 +21,7 @@ app.use((req, res, next) => {
     next();
 });
 
+// Indique les routes à utiliser pour un point End Point
 app.use("/api/books", bookRouter);
 
 module.exports = app;
