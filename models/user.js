6fc30963fc04a17fbd9bd6e69bd57881b79582d8
Maxime ROBIN <robin.maxime44@gmail.com>
2023-06-14 11:21:05 +0200

    fix schema error

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const userSchema = {
+const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
-}
+});
 
 userSchema.plugin(uniqueValidator);
 
