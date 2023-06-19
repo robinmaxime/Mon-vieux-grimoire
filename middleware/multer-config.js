97c92e990ba1a9dbb955a3668981b6a3b69dbf86
Maxime ROBIN <robin.maxime44@gmail.com>
2023-06-19 10:50:11 +0200

    Remove file extension in multer

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,5 +1,10 @@
 const multer = require('multer');
 
+
+function removeExtension(filename) {
+  return filename.substring(0, filename.lastIndexOf('.')) || filename;
+}
+
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
@@ -13,7 +18,7 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, removeExtension(name) + Date.now() + '.' + extension);
   }
 });
 
